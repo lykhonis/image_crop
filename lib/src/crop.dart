370ae738b6c2aadf5a3374f7b0ca33244ac55476
Maël Le Goff <legoffmael@gmail.com>
2022-12-21 12:35:50 +0900

    change: cropHandleSize depends of `disableResize`

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -6,7 +6,6 @@ const _kCropGridColor = Color.fromRGBO(0xd0, 0xd0, 0xd0, 0.9);
 const _kCropOverlayActiveOpacity = 0.3;
 const _kCropOverlayInactiveOpacity = 0.7;
 const _kCropHandleColor = Color.fromRGBO(0xd0, 0xd0, 0xd0, 1.0);
-const _kCropHandleSize = 10.0;
 const _kCropHandleHitSize = 48.0;
 const _kCropMinFraction = 0.1;
 
@@ -103,6 +102,8 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
         );
 
   bool get _isEnabled => _view.isEmpty == false && _image != null;
+
+  double get cropHandleSize => widget.disableResize ? 0.0 : 10.0;
 
   // Saving the length for the widest area for different aspectRatio's
   final Map<double, double> _maxAreaWidthMap = {};
@@ -202,6 +203,7 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
                 scale: _scale,
                 active: _activeController.value,
                 disableResize: widget.disableResize,
+                cropHandleSize: cropHandleSize,
               ),
             ),
           ),
@@ -237,7 +239,7 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
       return null;
     }
 
-    return size - const Offset(_kCropHandleSize, _kCropHandleSize) as Size;
+    return size - Offset(cropHandleSize, cropHandleSize) as Size;
   }
 
   Offset? _getLocalPoint(Offset point) {
@@ -352,7 +354,7 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
       boundaries.height * _area.top,
       boundaries.width * _area.width,
       boundaries.height * _area.height,
-    ).deflate(_kCropHandleSize / 2);
+    ).deflate(cropHandleSize / 2);
 
     if (widget.disableResize) {
       return _CropHandleSide.none;
@@ -639,6 +641,7 @@ class _CropPainter extends CustomPainter {
   final double scale;
   final double active;
   final bool disableResize;
+  final double cropHandleSize;
 
   _CropPainter({
     required this.image,
@@ -648,6 +651,7 @@ class _CropPainter extends CustomPainter {
     required this.scale,
     required this.active,
     required this.disableResize,
+    required this.cropHandleSize,
   });
 
   @override
@@ -663,10 +667,10 @@ class _CropPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final rect = Rect.fromLTWH(
-      _kCropHandleSize / 2,
-      _kCropHandleSize / 2,
-      size.width - _kCropHandleSize,
-      size.height - _kCropHandleSize,
+      cropHandleSize / 2,
+      cropHandleSize / 2,
+      size.width - cropHandleSize,
+      size.height - cropHandleSize,
     );
 
     canvas.save();
@@ -736,40 +740,40 @@ class _CropPainter extends CustomPainter {
 
     canvas.drawOval(
       Rect.fromLTWH(
-        boundaries.left - _kCropHandleSize / 2,
-        boundaries.top - _kCropHandleSize / 2,
-        _kCropHandleSize,
-        _kCropHandleSize,
+        boundaries.left - cropHandleSize / 2,
+        boundaries.top - cropHandleSize / 2,
+        cropHandleSize,
+        cropHandleSize,
       ),
       paint,
     );
 
     canvas.drawOval(
       Rect.fromLTWH(
-        boundaries.right - _kCropHandleSize / 2,
-        boundaries.top - _kCropHandleSize / 2,
-        _kCropHandleSize,
-        _kCropHandleSize,
+        boundaries.right - cropHandleSize / 2,
+        boundaries.top - cropHandleSize / 2,
+        cropHandleSize,
+        cropHandleSize,
       ),
       paint,
     );
 
     canvas.drawOval(
       Rect.fromLTWH(
-        boundaries.right - _kCropHandleSize / 2,
-        boundaries.bottom - _kCropHandleSize / 2,
-        _kCropHandleSize,
-        _kCropHandleSize,
+        boundaries.right - cropHandleSize / 2,
+        boundaries.bottom - cropHandleSize / 2,
+        cropHandleSize,
+        cropHandleSize,
       ),
       paint,
     );
 
     canvas.drawOval(
       Rect.fromLTWH(
-        boundaries.left - _kCropHandleSize / 2,
-        boundaries.bottom - _kCropHandleSize / 2,
-        _kCropHandleSize,
-        _kCropHandleSize,
+        boundaries.left - cropHandleSize / 2,
+        boundaries.bottom - cropHandleSize / 2,
+        cropHandleSize,
+        cropHandleSize,
       ),
       paint,
     );
